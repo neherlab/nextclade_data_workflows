304c21b7876b132baab2587c2a8eda885dd328a8
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-09-09 17:40:22 +0200

    feat: add recency of curated sequences

--- sars-cov-2/scripts/open_pango_curated.py
+++ sars-cov-2/scripts/open_pango_curated.py
@@ -1,4 +1,5 @@
 #%%
+from numpy import float64, int64
 import pandas as pd
 import datetime as dt
 #%%
@@ -51,8 +52,8 @@ dtype_dict = {
 #%%
 #download the master list of curated pangolin designations
 #https://www.pangolin.org/downloads/curated_designations.txt
-df = pd.read_csv('https://raw.githubusercontent.com/cov-lineages/pango-designation/master/lineages.csv')
-df
+df_pango = pd.read_csv('https://raw.githubusercontent.com/cov-lineages/pango-designation/master/lineages.csv')
+df_pango
 
 #%%
 # Load open data metadata into dataframe
@@ -63,42 +64,47 @@ df_gisaid = pd.read_csv('data/gisaid_metadata.tsv', sep='\t', dtype=dtype_dict)
 #%%
 #check for each curated strain whether it's found in the open metadata
 #maybe need to speed up this process by using a dictionary of open metadata
-df = df.join(df_gisaid.set_index('strain'), on='taxon',lsuffix='', rsuffix='_gisaid',how='left')
+df = df_pango.join(df_gisaid.set_index('strain'), on='taxon',lsuffix='', rsuffix='_gisaid',how='left')
 df = df.join(df_open.set_index('strain'), on='taxon',lsuffix='', rsuffix='_open',how='left')
+df['date_submitted'] = pd.to_datetime(df.date_submitted)
 df
 #%%
 df.columns.tolist()
 # %%
-df = df.assign(in_open=df.date_submitted.notnull().apply(lambda x: 'Yes' if x else 'No'))
+df = df.assign(in_open=df.date_submitted_open.apply(lambda x: 'Yes' if not type(x) == str else 'No'))
+df = df.assign(in_gisaid=df.date_submitted.apply('notna'))
+df.in_open.value_counts()
+#%%
+df.in_gisaid.value_counts()
 #%%
 # Output pivot table for each lineage with two columns one those sequences that are in open data and one for those that are not
-df_lin = df.pivot_table(index='lineage', columns="in_open", values='sequence_name', aggfunc='count')
-df_lin
-# %%
-df_lin['total'] = df_lin.sum(axis=1)
-df_lin['open_ratio'] = df_lin['Yes'] / df_lin.total
+df_lin = df.pivot_table(index='lineage', columns="in_open", aggfunc='count').taxon
 #%%
-df_gisaid[df_gisaid['strain'].str.contains('ONP180821NB32')]
+df_lin.to_csv('temp')
+# %%
+df_lin['No'] = df_lin.No.fillna(0).astype(int)
+df_lin['Yes'] = df_lin.Yes.fillna(0).astype(int)
+df_lin['total'] = df_lin.sum(axis=1).fillna(0).astype(int)
+df_lin['open_ratio'] = (df_lin['Yes'] / df_lin.total * 100).fillna(0).astype(int)
+df_lin.to_csv('temp')
+df_lin
 #%%
 # sort data frame by number of Yes downwards
 df_lin.sort_values('Yes', ascending=False, inplace=True)
-df_lin
-#%%
-# output to tsv
-df_lin.to_csv('other_data/curated_pango_open_stats.tsv', sep='\t')
-# %%
+df_lin.to_csv('temp')
 df_lin
 # %%
 # Add proportion of pango designation that is in the past 6 months
-df['recency'] = df.sample_date.apply(lambda x: 'Recent' if x > dt.datetime(2021,4,1) else 'Old')
-df
+def recency(x):
+    return 'NA' if not x['in_gisaid'] else('Recent' if x['date_submitted'] > dt.datetime(2021,4,1) else 'Old')
+
+df['recency'] = df.apply(recency,axis=1)
 #%%
-df_new = df.pivot_table(index='lineage', columns=["recency","in_open"], values='sample_date', aggfunc='count')
+df.recency.value_counts()
+#%%
+df_new = df.pivot_table(index='lineage', columns=["recency",'in_open'], values='taxon', aggfunc='count').fillna(0).astype(int)
+df_new.sort_values(("Recent","Yes"), ascending=False, inplace=True)
 df_new.to_csv('other_data/recent_curated_pango.tsv', sep='\t')
+df_new
 # %%
-for key, value in df.dtypes.items():
-    print(key, value)
-# %%
-for row in df_new.iterrows():
-    print(row)
-# %%
+df
