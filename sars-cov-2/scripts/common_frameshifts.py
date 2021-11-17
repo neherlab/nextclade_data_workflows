5d509d981afbec06f765b777538cd242abe12544
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-11-17 20:29:59 +0100

    fix: ignored most common stop due to 0/1 error

--- sars-cov-2/scripts/common_frameshifts.py
+++ sars-cov-2/scripts/common_frameshifts.py
@@ -1,21 +1,26 @@
-import pandas as pd
 import click
+import pandas as pd
+
 
 @click.command()
-@click.option('--number', default=20, help='Number of frameshifts')
-@click.option('--input-file', default="pre-processed/frameshifts.tsv", help='Input file')
+@click.option("--number", default=20, help="Number of frameshifts")
+@click.option("--input-file", default="pre-processed/frameshifts.tsv")
 def format(number, input_file):
     df = pd.read_csv(input_file, sep="\t")
+    df.dropna(inplace=True)
 
     #%%
-    for row in df[1:number].sort_values(by=['frame_shifts']).itertuples():
+    for row in df[0:number].sort_values(by=["frame_shifts"]).itertuples():
         fs = row.frame_shifts.split(",")[0].split(":")
         gene = fs[0]
         loc = fs[1].split("-")
         # beware: subtract 1 because in qc.json it's 0 indexed as opposed to nextclade output
-        start = int(loc[0])-1
+        start = int(loc[0]) - 1
         end = int(loc[1])
-        click.echo(f'{{ "geneName": "{gene}", "codonRange": {{"begin": {start}, "end": {end} }} }},')
+        click.echo(
+            f'{{ "geneName": "{gene}", "codonRange": {{"begin": {start}, "end": {end} }} }},'
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     format()
--- sars-cov-2/scripts/common_stops.py
+++ sars-cov-2/scripts/common_stops.py
@@ -1,23 +1,26 @@
-import pandas as pd
 import click
+import pandas as pd
+
 
 @click.command()
-@click.option('--number', default=20)
-@click.option('--input-file', default="pre-processed/stops.tsv", help='Input file')
+@click.option("--number", default=20)
+@click.option("--input-file", default="pre-processed/stops.tsv")
 def format(number, input_file):
     df = pd.read_csv(input_file, sep="\t")
+    df.dropna(inplace=True)
 
     output = []
 
-    for row in df[1:number].sort_values(by=['stops']).itertuples():
+    for row in df[0:number].sort_values(by=["stops"]).itertuples():
         stop = row.stops.split(":")
         gene = stop[0]
         # beware: subtract 1 because in qc.json it's 0 indexed as opposed to nextclade output
-        position = int(stop[1][1:-1])-1
+        position = int(stop[1][1:-1]) - 1
         # {"geneName": "ORF7a", "codon": 61},
 
         output.append(f'{{ "geneName": "{gene}", "codon": {position} }},')
     click.echo("\n".join(sorted(output)))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     format()
