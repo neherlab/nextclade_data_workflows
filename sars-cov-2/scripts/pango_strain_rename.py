519f2b4b98b148206ee2d2662e139ec30c48ee69
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-11-25 20:39:05 +0100

    fix: used variable pango_designation for two purposes

--- sars-cov-2/scripts/pango_strain_rename.py
+++ sars-cov-2/scripts/pango_strain_rename.py
@@ -7,30 +7,31 @@ import pandas as pd
 @click.command()
 @click.option("--metadata-strainnames", default="pre-processed/metadata_strainnames.tsv")
 @click.option("--pango-in", default="pre-processed/pango_raw.csv")
-@click.option("--pango-designations", default="pre-processed/pango_designations.tsv")
+@click.option("--pango-designations", default="pre-processed/pango_designations.csv")
 @click.option("--pango-designated-strains", default="pre-processed/pango_designated_strains.txt")
 def format(metadata_strainnames, pango_in, pango_designations, pango_designated_strains):
     meta_strains = pd.read_csv(metadata_strainnames, sep="\t")
-    pango_designations = pd.read_csv(pango_in, sep=",")
+    pango_des = pd.read_csv(pango_in, sep=",")
 
     alpha_digits_regex = re.compile("[^A-Z0-9/]")
 
-    pango_designations["canonical"] = pango_designations.taxon.apply(
+    pango_des["canonical"] = pango_des.taxon.apply(
         lambda x: alpha_digits_regex.sub("", x.upper())
     )
     meta_strains["canonical"] = meta_strains.strain.apply(
         lambda x: alpha_digits_regex.sub("", x.upper())
     )
 
-    pango_designations.set_index("canonical", inplace=True)
+    pango_des.set_index("canonical", inplace=True)
     meta_strains.set_index("canonical", inplace=True)
 
-    pango_designations = pango_designations.join(meta_strains, on="canonical")
+    pango_des = pango_des.join(meta_strains, on="canonical")
 
-    pango_designations.dropna(inplace=True)
+    pango_des.dropna(inplace=True)
 
-    pango_designations.to_csv(pango_designations, columns=["strain", "lineage"], index=False)
-    pango_designations.to_csv(
+    click.echo(pango_des)
+    pango_des.to_csv(pango_designations, columns=["strain", "lineage"], index=False)
+    pango_des.to_csv(
         pango_designated_strains, columns=["strain"], header=False, index=False
     )
 
