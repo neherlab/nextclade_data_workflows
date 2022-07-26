628f2f4207d025fd3e9d8fd2d3ce4f3f53d5f232
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-07-26 14:50:15 +0200

    fix: remove recombinants node_attrs -> name

--- sars-cov-2/scripts/remove_recombinants_from_auspice.py
+++ sars-cov-2/scripts/remove_recombinants_from_auspice.py
@@ -4,7 +4,7 @@ def main(input: str = "", output: str = ""):
     import json
     tree = json.load(open(input))
     tree["tree"]["children"][:] = [
-        child for child in tree["tree"]["children"] if child["node_attrs"] != "rec_parent"
+        child for child in tree["tree"]["children"] if child["name"] != "rec_parent"
     ]
     with open(output, 'w') as f:
         json.dump(tree, f, indent=2)
