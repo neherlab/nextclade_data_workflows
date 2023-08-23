052a3552c9b5073a3133793497fffb2fd85b3df2
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-08-24 00:25:51 +0200

    Overwrites for XBB.1.5.53 and children

--- sars-cov-2/scripts/update_virus_properties.py
+++ sars-cov-2/scripts/update_virus_properties.py
@@ -1,12 +1,18 @@
+"""
+This script makes updates to virus_properties when a new clade is added
+New clades require new characteristic mutations
+But we need to make sure we don't throw out other important information in virus properties
+Hence this script loads both old and new virus_properties.json and makes the necessary updates
+"""
 #%%
 import copy
 import json
-from collections import defaultdict
 
 #%%
-# path_old = "/Users/corneliusromer/code/nextclade_data/data/datasets/sars-cov-2-21L/references/BA.2/versions/2023-05-10T12:00:00Z/files/virus_properties.json"
-path_old = "/Users/corneliusromer/code/nextclade_data/data/datasets/sars-cov-2/references/MN908947/versions/2023-05-10T12:00:00Z/files/virus_properties.json"
-path_new = "/Users/corneliusromer/code/nextclade_data_workflows/virus_properties_new.json"
+path_old = "/Users/corneliusromer/code/nextclade_data/data/datasets/sars-cov-2-21L/references/BA.2/versions/2023-08-09T12:00:00Z/files/virus_properties.json"
+# path_old = "/Users/corneliusromer/code/nextclade_data/data/datasets/sars-cov-2/references/MN908947/versions/2023-08-09T12:00:00Z/files/virus_properties.json"
+path_new = "/Users/corneliusromer/code/nextclade_data_workflows/sars-cov-2/virus_properties.json"
+# path_out = "virus_properties_updated.json"
 path_out = path_old
 # Load old virus_properties.json
 old = json.load(open(path_old, "r"))
@@ -26,8 +32,12 @@ for k,v in new["nucMutLabelMap"].items():
 # Sort old["nucMutLabelMap"]
 old["nucMutLabelMap"] = dict(sorted(old["nucMutLabelMap"].items(), key=lambda item: int(item[0][:-1])))
 #%%
+# Don't use these mutations as "characteristic" as they are flip floppy
 for k in ["21T", "44T"]:
-    old["nucMutLabelMap"].pop(k)
+    try:
+        old["nucMutLabelMap"].pop(k)
+    except KeyError:
+        pass
 
 def reverse_a_dict(dict_of_lists):
     result = {}
