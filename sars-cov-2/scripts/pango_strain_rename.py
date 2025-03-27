c0257385216c3b77fc8d304f540e796268ab26c8
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-03-27 14:51:31 +0100

    faster

--- sars-cov-2/scripts/pango_strain_rename.py
+++ sars-cov-2/scripts/pango_strain_rename.py
@@ -14,7 +14,7 @@ import pandas as pd
 ALPHA_DIGITS_REGEX = re.compile("[^A-Z0-9/-]")
 
 
-def process_dataframe(filepath, sep, col_to_canonicalize):
+def process_dataframe(filepath, sep, col_to_canonicalize, col_to_keep=None):
     """Reads, canonicalizes, and drops duplicates for one input DataFrame."""
     df = pd.read_csv(filepath, sep=sep)
     df["canonical"] = (
@@ -24,12 +24,8 @@ def process_dataframe(filepath, sep, col_to_canonicalize):
         .str.replace(ALPHA_DIGITS_REGEX, "", regex=True)
     )
     df.drop_duplicates(subset=["canonical"], keep="first", inplace=True)
-
-    # Determine columns to keep for merging
-    cols_to_keep = ["canonical", col_to_canonicalize]
-    if "lineage" in df.columns:  # Keep lineage only if it exists (for pango_des)
-        cols_to_keep.append("lineage")
-    return df[cols_to_keep]
+    df.set_index("canonical", inplace=True)
+    return df[[col_to_keep]]
 
 
 @click.command()
@@ -47,22 +43,25 @@ def format(
     with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
         # Submit processing tasks for both files
         future_meta = executor.submit(
-            process_dataframe, metadata_strainnames, "\t", "strain"
+            process_dataframe, metadata_strainnames, "\t", "strain", "strain"
         )
-        future_pango = executor.submit(process_dataframe, pango_in, ",", "taxon")
+        future_pango = executor.submit(process_dataframe, pango_in, ",", "taxon", "lineage")
         # Get results (processed DataFrames)
         meta_strains = future_meta.result()
         pango_des = future_pango.result()
 
-    merged_data = pd.merge(
-        pango_des[["lineage", "canonical"]],
-        meta_strains[["strain", "canonical"]],
-        on="canonical",
-        how="inner",
+    # merged_data = pd.merge(
+    #     pango_des[["lineage", "canonical"]],
+    #     meta_strains[["strain", "canonical"]],
+    #     on="canonical",
+    #     how="inner",
+    # )
+    joined = meta_strains.join(
+        pango_des, how="inner"
     )
 
-    merged_data[["strain", "lineage"]].to_csv(pango_designations, index=False)
-    merged_data[["strain"]].to_csv(pango_designated_strains, header=False, index=False)
+    joined[["strain", "lineage"]].to_csv(pango_designations, index=False)
+    joined[["strain"]].to_csv(pango_designated_strains, header=False, index=False)
 
 
 if __name__ == "__main__":
