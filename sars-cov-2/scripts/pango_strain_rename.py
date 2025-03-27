45662db445bc911d121aa37840c51e3aa9333ec7
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-03-27 14:57:54 +0100

    thread instead of process is faster

--- sars-cov-2/scripts/pango_strain_rename.py
+++ sars-cov-2/scripts/pango_strain_rename.py
@@ -19,11 +19,10 @@ def process_dataframe(filepath, sep, col_to_canonicalize, col_to_keep=None):
     df = pd.read_csv(filepath, sep=sep)
     df["canonical"] = (
         df[col_to_canonicalize]
-        .astype(str)
-        .str.upper()
         .str.replace(ALPHA_DIGITS_REGEX, "", regex=True)
+        .str.upper()
     )
-    df.drop_duplicates(subset=["canonical"], keep="first", inplace=True)
+    df.drop_duplicates(subset=["canonical"], keep="last", inplace=True)
     df.set_index("canonical", inplace=True)
     return df[[col_to_keep]]
 
@@ -40,25 +39,18 @@ def process_dataframe(filepath, sep, col_to_canonicalize, col_to_keep=None):
 def format(
     metadata_strainnames, pango_in, pango_designations, pango_designated_strains
 ):
-    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
         # Submit processing tasks for both files
         future_meta = executor.submit(
             process_dataframe, metadata_strainnames, "\t", "strain", "strain"
         )
-        future_pango = executor.submit(process_dataframe, pango_in, ",", "taxon", "lineage")
-        # Get results (processed DataFrames)
+        future_pango = executor.submit(
+            process_dataframe, pango_in, ",", "taxon", "lineage"
+        )
         meta_strains = future_meta.result()
         pango_des = future_pango.result()
 
-    # merged_data = pd.merge(
-    #     pango_des[["lineage", "canonical"]],
-    #     meta_strains[["strain", "canonical"]],
-    #     on="canonical",
-    #     how="inner",
-    # )
-    joined = meta_strains.join(
-        pango_des, how="inner"
-    )
+    joined = meta_strains.join(pango_des, how="inner")
 
     joined[["strain", "lineage"]].to_csv(pango_designations, index=False)
     joined[["strain"]].to_csv(pango_designated_strains, header=False, index=False)
