7267cbe9d1372a44d437362ae5e9b20deb655b72
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-03-27 17:02:07 +0100

    Fix merge conflicts

--- sars-cov-2/profiles/cluster/submit.sh
+++ sars-cov-2/profiles/cluster/submit.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #SBATCH --output=log/%j.out                 # where to store the output ( %j is the JOBID )
 #SBATCH --error=log/%j.err                  # where to store error messages
 
 # activate conda environment
-source /scicore/home/neher/roemer0001/miniconda3/etc/profile.d/conda.sh
 conda activate py310simple
+. /scicore/home/neher/roemer0001/miniconda3/etc/profile.d/conda.sh
 export AUGUR_MINIFY_JSON=1
 export AUGUR_RECURSION_LIMIT=10000
 
